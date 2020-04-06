4928dc1c1b3e592c733e01213388759397069b5b
chihiro <fujisawachihiro@gmail.com>
2020-04-06 19:33:39 +0900

    ゲーム情報を表示

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -1,14 +1,23 @@
 import React, { FC } from 'react';
+import { Item } from 'semantic-ui-react'
 import games from '../data/game.json'
 
 const GameList: FC = () => {
-  let list = games.game.map((game, index) => <li key={index}>{game.name}</li>)
 
   return (
     <div>
-      <ul>
-        {list}
-      </ul>
+      <Item.Group>
+        { games.game.map(game => (
+          <Item>
+            <Item.Content>
+              <Item.Header>{game.name}</Item.Header>
+              <Item.Meta>{game.release_date}</Item.Meta>
+              <Item.Meta>{game.hardware}</Item.Meta>
+              <Item.Meta>{game.selling_agency}</Item.Meta>
+            </Item.Content>
+          </Item>
+        ))}
+      </Item.Group>
     </div> );
 }
 
