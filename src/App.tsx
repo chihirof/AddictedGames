58a9986f36b1df2d37f3aa3982ac913ed6e548e2
chihiro <fujisawachihiro@gmail.com>
2020-04-08 21:54:42 +0900

    レスポンシブデザインに対応

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
 import React, { FC } from 'react';
 import Header from './components/Header'
 import GameList from './components/GameList'
+import { Responsive } from 'semantic-ui-react'
 
 const title = "chihiroがハマってきたゲームたち"
 const description = "chihiroがハマってきたゲームを覚えている限りで紹介させてもらいます。"
 
 const appStyle = {
   container: {
-    width: 600,
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    marginTop: 100,
-    marginBottom: 100
+    pc: {
+      width: 750,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      marginTop: 100,
+      marginBottom: 100
+    },
+    phone: {
+      marginLeft: 15,
+      marginRight: 15,
+      marginTop: 100,
+      marginBottom: 100
+    }
   }
 }
 
 const App: FC = () => {
   return (
-    <div style={appStyle.container}>
-      <Header title={title} description={description}/>
-      <GameList />
-    </div>
+    <div>
+      <Responsive minWidth={768}>
+        <div style={appStyle.container.pc}>
+          <Header title={title} description={description}/>
+          <GameList />
+        </div>
+      </Responsive>
+      <Responsive maxWidth={767}>
+      <div style={appStyle.container.phone}>
+        <Header title={title} description={description}/>
+        <GameList />
+      </div>
+    </Responsive>
+  </div>
   );
 }
 
