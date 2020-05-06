e3ae8062820b84c23473ee46cb62102830c3d44c
chihiro <fujisawachihiro@gmail.com>
2020-05-06 14:40:03 +0900

    ヘッダー部分を横並びにした

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 
 const Header:FC = () => {
   return (
-    <div>
+    <div className="header">
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/game">Game</Link></li>
